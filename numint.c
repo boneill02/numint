33e6b3b0820825f630a9d38594491d1a7018f0eb
Ben O'Neill <ben@benoneill.xyz>
2021-10-31 14:41:25 -0400

    finished?

--- numint.c
+++ numint.c
@@ -20,7 +20,7 @@ void clear_string(char *s, int len) {
 /* gets a longint from standard input. */
 long getn(char *prompt) {
 	char *ptr, line[128];
-	long n;
+	long n = 0;
 	printf("%s: ", prompt);
 	while (fgets(line, sizeof(line), stdin)) {
 		n = strtol(line, &ptr, 10);
@@ -49,16 +49,38 @@ double evaluate(Polynomial p, double x) {
 
 double midpoint_approx(Polynomial p, int ll, int ul, int num_si) {
 	double width = ((double) (ul - ll)) / num_si;
-	printf("width: %.2f\n", width);
 
 	double res = 0.0;
 	for (int i = 0; i < num_si; i++) {
-		double midpoint = ll + (((width * i) + (width * (i + 1))) / 2);
+		double x1 = width * i;
+		double midpoint = ll + (x1 + (width / 2));
 		res += evaluate(p, midpoint);
 	}
 
 	res *= width;
 
+	return res;
+}
+
+double trapezoid_approx(Polynomial p, int ll, int ul, int num_si) {
+	double width = ((double) (ul - ll)) / num_si;
+
+	double res = 0.0;
+	for (int i = 0; i < num_si; i++) {
+		double x1 = ll + width * i;
+		double x2 = ll + width * (i + 1);
+		res += (evaluate(p, x1) + evaluate(p, x2)) / 2;
+	}
+
+	res *= width;
+	return res;
+}
+
+double simpson_approx(Polynomial p, int ll, int ul, int num_si) {
+	double ma = midpoint_approx(p, ll, ul, num_si);
+	double ta = trapezoid_approx(p, ll, ul, num_si);
+
+	double res = ((2.0 * ma) + ta) / 3.0;
 	return res;
 }
 
@@ -84,14 +106,17 @@ int main(int argc, char *argv[]) {
 	p.constant = getn(s);
 	free(s);
 
-	/* calculate test */
-	double val = evaluate(p, 0.5);
-	printf("(0.5, %.2f)\n", val);
-
 	/* do approximations */
-	double mp = midpoint_approx(p, lower_limit, upper_limit,
+	double midpoint = midpoint_approx(p, lower_limit, upper_limit,
 			num_subints);
-	printf("Midpoint approximation: %.10f\n", mp);
+	double trapezoid = trapezoid_approx(p, lower_limit, upper_limit,
+			num_subints);
+	double simpson = simpson_approx(p, lower_limit, upper_limit,
+			num_subints);
+
+	printf("Midpoint approximation: %.10f\n", midpoint);
+	printf("Trapezoid approximation: %.10f\n", trapezoid);
+	printf("Simpson's approximation: %.10f\n", simpson);
 
 	return 0;
 }
